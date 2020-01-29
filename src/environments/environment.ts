8b698cdb7deb2ee98c808a4b2f5c495112227643
KirrMehBabeh <5592@ait-sydney.net.au>
2020-01-29 15:51:44 +1100

    dependancies

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyB8vvNUBR7Vvj29nNvGvc_vtsrLv9ihDK8",
+    authDomain: "assignment-7993f.firebaseapp.com",
+    databaseURL: "https://assignment-7993f.firebaseio.com",
+    projectId: "assignment-7993f",
+    storageBucket: "assignment-7993f.appspot.com",
+    messagingSenderId: "909958398708",
+    appId: "1:909958398708:web:638d27b95001c7cfac94a1",
+    measurementId: "G-MVKX72Q1H7"
+  }
 };
 
 /*
